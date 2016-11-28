556e703652c0e9f81dce7499cc6ee6bd6c2a9f69
Vftdan <Vftdan.feed@gmail.com>
2016-11-28 13:24:34 +0300

    Update favicon.js

--- sanmarino/favicon.js
+++ sanmarino/favicon.js
@@ -1,1 +1,1 @@
-(function(id){var ic=document.getElementById(id);alert(ic);var i=false;ic.setAttribute("type","image/png");setInterval(function(){i=!i;ic.setAttribute("href","favicon_"+i+".png");},3000);})("icon")
+(function(id){var ic=document.getElementById(id);var i=false;ic.setAttribute("type","image/png");setInterval(function(){i=!i;ic.setAttribute("href","favicon_"+i+".png");alert(i);},3000);})("icon")
